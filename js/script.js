a7089e898ead2bc43ad4546a286515be0f187982
Henri Viigimäe <henri.viigim2e@gmail.com>
2022-04-15 02:07:58 +0300

    Add pencil, rainbow and eraser functions

--- js/script.js
+++ js/script.js
@@ -1,22 +1,11 @@
 const GRID = document.querySelector('#grid');
 const BUTTONS = document.querySelectorAll('button');
 const DEFAULT_GRID = 24;
+let divColor = 'rgb(255,255,255)'; // Default grid color
+let isDrawing = true;
+let colorChoice = ''; // User needs to pick an option from the menu to start drawing
 
 window.onload = createGrid(DEFAULT_GRID); // Create initial grid when user loads the page
-
-for(let i = 0; i<BUTTONS.length; i++){
-    switch(BUTTONS[i].getAttribute('id')){
-        case 'clear':
-            BUTTONS[i].addEventListener('click', clearGrid);
-        break;
-        case 'gridSize':
-            BUTTONS[i].addEventListener('click', getSize);
-        break;
-        case 'toggleBorder':
-            BUTTONS[i].addEventListener('click', toggleBorder);
-        break;
-    }
-}
 
 function createGrid(size){
     removeExistingGrid();
@@ -30,12 +19,82 @@ function removeExistingGrid(){
 }
 
 function populateGrid(squares){
-    GRID.style.gridTemplate = `repeat(${squares}, 1fr) / repeat(${squares}, 1fr)`
+    GRID.style.gridTemplate = `repeat(${squares}, 1fr) / repeat(${squares}, 1fr)`;
     for(let i = 0; i<squares ** 2;  i++){
         let div = document.createElement('div')
         div.classList.add('divBg', 'divBorder')
+        div.setAttribute('draggable', 'false');
         GRID.appendChild(div);
+        div.addEventListener('mouseover', colorSquares);
     }
+}
+
+function colorSquares(e){
+    if(isDrawing){
+        switch(colorChoice){
+            case 'pencil':
+                divColor = 'rgb(60,60,60)';
+                e.target.style.backgroundColor = divColor;
+            break;
+            case 'eraser':
+                divColor = 'rgb(255,255,255)';
+                e.target.style.backgroundColor = divColor;
+            break;
+            case 'rainbow':
+                divColor = `rgb(
+                                ${Math.floor(Math.random()*256)},
+                                ${Math.floor(Math.random()*256)},
+                                ${Math.floor(Math.random()*256)}
+                            )`;
+                e.target.style.backgroundColor = divColor;
+            break;
+            case 'shading':
+                divColor = 'rgb(60,60,60)';
+                e.target.style.backgroundColor = divColor;
+            break;
+        }
+    }
+}
+
+/* Add functionality to the user interface*/
+
+for(let i = 0; i<BUTTONS.length; i++){
+    let btn = BUTTONS[i];
+    switch(btn.getAttribute('id')){
+        case 'pencil':
+            btn.addEventListener('click', () => colorChoice = 'pencil');
+            btn.addEventListener('click', toggleStates);
+        break;
+        case 'eraser':
+            btn.addEventListener('click', () => colorChoice = 'eraser');
+            btn.addEventListener('click', toggleStates);
+        break;
+        case 'rainbow':
+            btn.addEventListener('click', () => colorChoice = 'rainbow');
+            btn.addEventListener('click', toggleStates);
+        break
+        case 'shading':
+            btn.addEventListener('click', () => colorChoice = 'shading');
+            btn.addEventListener('click', toggleStates);
+        break;
+        case 'clear':
+            btn.addEventListener('click', clearGrid);
+        break;
+        case 'gridSize':
+            btn.addEventListener('click', getSize);
+        break;
+        case 'toggleBorder':
+            btn.addEventListener('click', toggleBorder);
+        break;
+    }
+}
+
+function toggleStates(e){
+    for(let i = 0; i<BUTTONS.length; i++){  // If present, remove the "selected" class from all elements
+        BUTTONS[i].classList.remove('selected')
+        }
+    e.target.classList.add('selected'); // If applicable, add the "selected" class to the element that was clicked
+
 }
 
 function clearGrid(){
@@ -44,9 +103,9 @@ function clearGrid(){
     }
 }
 function getSize(){
-    let squares = Number(prompt('Type a size ranging from 2-64'));
+    let squares = Number(prompt('Enter a size ranging from 2-64'));
     if(squares === NaN || squares > 64 || squares < 2){
-        alert('Invalid input. Try typing a value from 2-64');
+        alert('Invalid input. Enter a value between 2-64');
         return;
     }
     createGrid(squares);
